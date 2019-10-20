bf44087e7c23e87f693303811516471f6269e169
Frank Deng <frank.deng@oracle.com>
2019-10-20 00:14:06 -0700

    Fix kube-prometheus deployment

--- deploy_kube-prometheus.sh
+++ deploy_kube-prometheus.sh
@@ -4,16 +4,17 @@
 # usage:
 # run this script on the machine where you install and configure kubectl
 
-set -ex
+set -xe
 
 rm -rf kube-prometheus
 
 git clone https://github.com/coreos/kube-prometheus.git
 
-kubectl create -f kube-prometheus/manifests/
+kubectl create -f kube-prometheus/manifests/ || :
 
 sleep 30
 kubectl get crds | grep  monitoring
 
-# need to re-try here
-kubectl create -f kube-prometheus/manifests/
+# expect some failure above
+# so we re-try here
+kubectl create -f kube-prometheus/manifests/ || :
