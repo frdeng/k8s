cda11a59fdda7171afeea0b0edef8e49f133e967
Frank Deng <frank.deng@oracle.com>
2019-10-23 09:29:50 -0700

    Add weave network plugin option

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -4,18 +4,21 @@
 
 # setup kubenetes cluster with kubeadm
 # 1 master, 2+ worker nodes all running Centos 7
-# The script creates latest kubenetes, also deploys Calico pod network add-on.
-# tested: k8s v1.15.4, -  v1.16.1
+# The script creates latest kubenetes, also deploys flannel pod network add-on by default.
+# tested: k8s v1.15.4, -  v1.16.2
 
 # Usage:
 
 # 1. modify instance public IPs below, and login user name, the login user should be able to sudo without password.
-# 2. setup ssh public key authenticatoin from your linux machine to the 3 instances.
-# 3. run this script on you linux macine
+# 2. setup ssh public key authenticatoin from your machine to your instances.
+# 3. run this script on you machine, options:
+#   $0 - create k8s cluster
 #   $0 reset - clean up k8s cluster on all nodes
-#   $0 create k8s cluster
+#   $0 reboot - reboot all nodes
 
 set -xe
+#### Custom configuration
+
 # install specific version k8s
 # KVER=1.16.1-0
 
@@ -33,26 +36,42 @@ NODE_PUBLIC_IPS="$NODE0_PUBLIC_IP $NODE1_PUBLIC_IP"
 # master and worker nodes hostname
 # MASTER=frdeng-master
 # domainname
-#DOMAINNAME=sub05190107241.lsavcn.oraclevcn.co
+# DOMAINNAME=sub05190107241.lsavcn.oraclevcn.com
 
 # master and worker nodes private IP
 # MASTER_IP=10.0.1.114
-#
 
-# pod network addons
+# pod network add-ons
+# Default: Flannel
+FLANNEL_POD_NETWORK="10.244.0.0/16"
+FLANNEL_YML="https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
+
+# Calico
 CALICO_POD_NETWORK="192.168.0.0/16"
-#CALICO_YML="https://docs.projectcalico.org/v3.8/manifests/calico.yaml"
 CALICO_YML="https://docs.projectcalico.org/v3.9/manifests/calico.yaml"
 
-FLANNEL_POD_NETWORK="10.244.0.0/16"
-#FLANNEL_YML="https://raw.githubusercontent.com/coreos/flannel/62e44c867a2846fefb68bd5f178daf4da3095ccb/Documentation/kube-flannel.yml"
-FLANNEL_YML="https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
+# Weave, see https://www.weave.works/docs/net/latest/kubernetes/kube-addon/
+WEAVE_POD_NETWORK="192.168.0.0/16"
+#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+# curl "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" to see what it redirects to
+WEAVE_YML="https://cloud.weave.works/k8s/v1.10/net.yaml"
 
 #pod_network=$CALICO_POD_NETWORK
 #network_yml=$CALICO_YML
 
+# use flannal by default
 pod_network=$FLANNEL_POD_NETWORK
 network_yml=$FLANNEL_YML
+
+# uncomment below for Calico
+#pod_network=$CALICO_POD_NETWORK
+#network_yml=$CALICO_YML
+
+# uncomment below for Weave
+#pod_network=$WEAVE_POD_NETWORK
+#network_yml=$WEAVE_YML
+
+#########
 
 # $1=ip
 # $2=cmd
@@ -63,7 +82,7 @@ ssh_cmd() {
 #### reset
 if [ "$1" = reset ]; then
     cmd0="sudo kubeadm reset -f; sudo systemctl restart kubelet; rm -fr \$HOME/.kube; sudo rm -rf /var/log/pods; rm -rf ~/.helm"
-    # remove the packags in order to install old version
+    # remove the packags in order to install specified version
     cmd1="sudo yum -y remove kubelet kubeadm kubectl"
     for ip in $CLUSTER_PUBLIC_IPS; do
         for cmd in "$cmd0" "$cmd1"; do
@@ -86,22 +105,23 @@ fi
 # Set SELinux in permissive mode (effectively disabling it)
 cmd_disable_selinux="sudo setenforce 0; sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config"
 
-# disable firewalld
+# stop and disable firewalld
 cmd_disable_firewalld="sudo systemctl stop firewalld; sudo systemctl disable firewalld"
 
 # remove swap
 cmd_swap="sudo sed -i '/swap/d' /etc/fstab; sudo swapoff -a"
 
 # disable ipv6
-cmd_ipv6="sudo bash -c 'cat <<EOF > /etc/sysctl.d/ipv6.conf
-net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.default.disable_ipv6 = 1
-EOF
-';
-sudo sysctl --system"
+# cmd_ipv6="sudo bash -c 'cat <<EOF > /etc/sysctl.d/ipv6.conf
+# net.ipv6.conf.all.disable_ipv6 = 1
+# net.ipv6.conf.default.disable_ipv6 = 1
+# EOF
+# ';
+# sudo sysctl --system"
 
 for ip in $CLUSTER_PUBLIC_IPS; do
-    for cmd in "$cmd_disable_selinux" "$cmd_disable_firewalld" "$cmd_swap" "$cmd_ipv6"; do
+    #for cmd in "$cmd_disable_selinux" "$cmd_disable_firewalld" "$cmd_swap" "$cmd_ipv6"; do
+    for cmd in "$cmd_disable_selinux" "$cmd_disable_firewalld" "$cmd_swap"; do
         ssh_cmd $ip "$cmd"
     done
 done
@@ -143,7 +163,7 @@ done
 #    done
 #done
 
-# Flannel specific config for bridge, pass bridge traffic for flannel
+# Network specific config for bridge, pass bridge traffic for flannel, weave
 cmd_sysctl="sudo bash -c 'cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
@@ -171,6 +191,7 @@ cmd="mkdir -p \$HOME/.kube;
      sudo chown \$(id -u):\$(id -g) \$HOME/.kube/config"
 ssh_cmd $MASTER_PUBLIC_IP "$cmd"
 
+# add kubectl completion and aliases to ~/.bashrc
 cmd="grep -q 'kubectl completion bash'  ~/.bashrc || cat >> ~/.bashrc <<EOF
 command -v kubectl &>/dev/null && source <(kubectl completion bash)
 alias k=kubectl
