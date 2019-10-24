360b31b0e6b56aceaa22d09c49a97efe999ed3d5
Frank Deng <frank.deng@oracle.com>
2019-10-23 17:55:45 -0700

    fix istio install with jaeger and grafana

--- install_istio.sh
+++ install_istio.sh
@@ -27,24 +27,31 @@ if [ "$1" = install ]; then
     export ISTIO_VERSION=$(ls -d istio-* | sed 's/istio-//')
     sudo cp -f istio-$ISTIO_VERSION/bin/istioctl /usr/local/bin
 
-    # helm install istio
+    # helm install istio-init
     helm install istio-$ISTIO_VERSION/install/kubernetes/helm/istio-init --name istio-init --namespace istio-system \
-    --set tracing.enabled=true \ # enable jaeger
-    --set grafana.enabled=true # enable grafana
 
-    # wait until all CRDs created
+    # wait until all CRDs are created
     sleep 10
-    kubectl get crds | grep istio.io
-    helm install istio-$ISTIO_VERSION/install/kubernetes/helm/istio --name istio --namespace istio-system
+    while ! kubectl get crds | grep istio.io; do
+        sleep 10
+    done
+
+    # install istio  with jaeger and grafana enabled
+    helm install istio-$ISTIO_VERSION/install/kubernetes/helm/istio --name istio --namespace istio-system \
+    --set tracing.enabled=true \
+    --set grafana.enabled=true
     sleep 10
     kubectl -n istio-system get pods
+
     # set default istio injection to default ns
-    kubectl label namespace default istio-injection=enabled
+    kubectl label namespace default istio-injection=enabled || :
+
 elif [ "$1" = uninstall ]; then
     helm delete --purge istio || :
     helm delete --purge istio-init || :
     helm delete --purge istio-cni || :
     kubectl delete namespace istio-system || :
+    kubectl delete --ignore-not-found -f $ISTIO_VERSION/install/kubernetes/helm/istio-init/files/
 elif [ "$1" = bookinfo ]; then
     ISTIO_VERSION=$(ls -d istio-* | sed 's/istio-//')
     # suppose default ns has label: istio-injection=enabled
