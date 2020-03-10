082770cba4cd2f1e56e07f327fd6b8337839c6e6
Frank Deng <frank.deng@oracle.com>
2020-03-10 10:01:03 -0700

    Fix for helm 3

--- install_helm.sh
+++ install_helm.sh
@@ -4,7 +4,7 @@
 
 # usage:
 # $0 - run this script on the machine where you install and configure kubectl
-# $0 uninstall - to uninstall helm
+# $0 uninstall - to uninstall helm, helm 2 only
 
 set -ex
 
@@ -12,7 +12,7 @@ set -ex
 VER=3
 
 ### uninstall
-if [ "$1" = uninstall ]; then
+if [ "$1" = uninstall -a "$VER" = 2 ]; then
     helm reset
     # wait untill tiller is gone
     while kubectl get -n kube-system pods | grep tiller-deploy; do
@@ -30,33 +30,33 @@ if [ "$VER" = "2"  ]; then
     #curl -L https://git.io/get_helm.sh > get_helm.sh
     #chmod +x get_helm.sh
     #./get_helm.sh --version $VER
+    # helm init: deploy tiller
+
+    #if [ "$VER" = v2.14.3 ]; then
+    # helm v2.14.3 init doesn't work with k8s 1.16 due to the API version change
+    # workaround
+    #    helm init --service-account tiller --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | sed 's@  replicas: 1@  replicas: 1\n  selector: {"matchLabels": {"app": "helm", "name": "tiller"}}@' | kubectl apply -f -
+    # fi
+
+    helm init --service-account tiller
+    # create service account: tiller
+    kubectl create serviceaccount --namespace kube-system tiller
+    kubectl get -n kube-system serviceaccounts tiller
+
+    # and grant cluster admin permission to tiller
+    # as the prometheus, istio helm chart installation requires the permission
+    kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+
+    kubectl get clusterrolebinding tiller-cluster-rule
+
+    # wait until tiller pod running
+    while ! kubectl get -n kube-system pods | grep tiller-deploy | grep Running; do
+        sleep 10
+    done
 elif [ "$VER" = "3" ]; then
     curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 fi
 
-# helm init: deploy tiller
-
-#if [ "$VER" = v2.14.3 ]; then
-# helm v2.14.3 init doesn't work with k8s 1.16 due to the API version change
-# workaround
-#    helm init --service-account tiller --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | sed 's@  replicas: 1@  replicas: 1\n  selector: {"matchLabels": {"app": "helm", "name": "tiller"}}@' | kubectl apply -f -
-# fi
-
-helm init --service-account tiller
-# create service account: tiller
-kubectl create serviceaccount --namespace kube-system tiller
-kubectl get -n kube-system serviceaccounts tiller
-
-# and grant cluster admin permission to tiller
-# as the prometheus, istio helm chart installation requires the permission
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-
-kubectl get clusterrolebinding tiller-cluster-rule
-
-# wait until tiller pod running
-while ! kubectl get -n kube-system pods | grep tiller-deploy | grep Running; do
-    sleep 10
-done
 helm version
 
 # add bash completion
